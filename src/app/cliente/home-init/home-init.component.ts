0949882513706c54b2fc22b80101292ef14676b7
rafael-bastidas <70807890+rafael-bastidas@users.noreply.github.com>
2022-07-13 12:06:48 -0400

    Update

--- src/app/cliente/home-init/home-init.component.ts
+++ src/app/cliente/home-init/home-init.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { GenericService } from 'src/app/servicios/generic.service';
+import { TInfoCondominio } from 'src/app/utils/models/interfaces';
 
 @Component({
   selector: 'app-home-init',
@@ -7,9 +12,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeInitComponent implements OnInit {
 
-  constructor() { }
+  suscritions: Subscription[] = []
+  allCond: TInfoCondominio[] = []
+  selectCond:FormControl = new FormControl('')
+  
+  constructor(private connectHttp: GenericService, private router: Router) { }
 
   ngOnInit(): void {
+    this.getListCond()
+    this.selectCond.valueChanges.subscribe(value => {value !== '' ? this.goLogin(value) : ''})
   }
+
+  getListCond(){
+    this.suscritions[0] = this.connectHttp.post({
+     url:"general", params:"GET-LIST-COND", data:localStorage.getItem("tokenUser")
+   }).subscribe(result => {
+     if (!result.error) {
+       this.allCond = result.data
+     }
+   });
+ }
+
+ goLogin(code:string){
+  this.router.navigate([`/${code}/login`])
+ }
 
 }
